5676c5039d792d656a19883992618b38b02976d8
arthuryac <131703006+arthuryac@users.noreply.github.com>
2025-07-03 08:39:56 +0200

    Add files via upload

---
+++ App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import VehicleTable from './components/VehicleTable';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Dashboard Location de Véhicules</h1>
+      <VehicleTable />
+    </div>
+  );
+}
+
+export default App;
